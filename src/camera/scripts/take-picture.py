0cab33df81d8d200e67c3ccc5227a260501adbe8
Frederik Held <git@frederikheld.de>
2021-02-25 22:11:10 +0100

    Can search/post via Twitter API (on dev computer for faster development)

--- src/camera/scripts/take-picture.py
+++ src/camera/scripts/take-picture.py
@@ -1,15 +1,63 @@
 from time import sleep
-from picamera import PiCamera
+# from picamera import PiCamera
+import json
+from TwitterAPI import TwitterAPI
 
-print("Hello again!")
+### READ CONFIG ###
 
-try:
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    sleep(2) # Camera warm-up time
-    camera.capture('foo.jpg')
-except:
-    print("Error: ", sys.exc_info()[0])
-    raise
+with open("config.json") as json_config_file:
+    CONFIG = json.load(json_config_file)
+
+
+### FUNCTIONS ###
+
+def take_picture():
+    print("take_picture(): picture taken (mocked)")
+
+# def take_picture():
+#     try:
+#         camera = PiCamera()
+#         camera.resolution = (1024, 768)
+#         camera.start_preview()
+#         sleep(2) # Give camera some time to get ready
+#         camera.capture('foo.jpg')
+#     except:
+#         print("Error: ", sys.exc_info()[0])
+#         raise
     
+def upload_picture():
+    print("upload_picture() not implemented yet!")
+
+def tweet_picture():
+
+    twitter = TwitterAPI(
+        CONFIG["twitter"]["api_key"],
+        CONFIG["twitter"]["api_key_secret"],
+        CONFIG["twitter"]["access_token"],
+        CONFIG["twitter"]["access_token_secret"]
+    )
+
+    # Request a tweet:
+
+    response = twitter.request('search/tweets', {'q': '#JustDevThings'})
+    print(response.status_code)
+    
+    for tweet in response.get_iterator():
+        if 'text' in tweet:
+            print(tweet['text'])
+
+    # Post a tweet:
+
+    response = twitter.request('statuses/update', {'status': 'Hello World! Part 3.'})
+    print (response.status_code)
+
+
+### MAIN ###
+
+print("Hello from Python!")
+
+take_picture()
+
+upload_picture()
+
+tweet_picture()
