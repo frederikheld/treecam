26fbd8ad1d3cc7b43f91421c769a351dbfc5d7aa
Frederik Held <git@frederikheld.de>
2021-02-26 02:33:36 +0100

    Can tweet threads now

--- src/camera/scripts/take-picture.py
+++ src/camera/scripts/take-picture.py
@@ -39,7 +39,66 @@ def take_picture():
 def upload_picture(file_path):
     print("upload_picture() not implemented yet!")
 
-def tweet_picture(file_path):
+def tweet_picture(file_path, status, in_reply_to_status_id = None):
+    """
+        This function can tweet a picture with a status message.
+
+        arguments:
+            file_path: path to the picture that shall be tweeted
+            status: status text of the tweet
+            in_reply_to_status_id: (optional) status_id of a tweet to which this tweet will be a response
+
+        The function returns a json object with the keys 'error' and 'response'.
+            'error': False if successfully tweeted. Otherwise True.
+            'response': json representation of Twitter API response object [1]
+
+        [1]: see: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
+
+    """
+
+    twitter = TwitterAPI(
+        CONFIG["twitter_cam"]["secrets"]["api_key"],
+        CONFIG["twitter_cam"]["secrets"]["api_key_secret"],
+        CONFIG["twitter_cam"]["secrets"]["access_token"],
+        CONFIG["twitter_cam"]["secrets"]["access_token_secret"]
+    )
+
+    # upload image:
+
+    file = open(file_path, 'rb')
+    picture = file.read()
+
+    response = twitter.request(
+        'media/upload',
+        None,
+        { 'media': picture }
+    )
+
+    if response.status_code != 200:
+        return {
+            'error': True,
+            'response': response.json()
+        }
+
+    # tweet status with reference to uploaded picture:
+
+    media_id = response.json()['media_id']
+
+    response = twitter.request(
+        'statuses/update',
+        {
+            'status': status,
+            'media_ids': media_id,
+            'in_reply_to_status_id': in_reply_to_status_id
+        }
+    )
+    
+    return {
+        'error': response.status_code != 200,
+        'response': response.json()
+    }
+
+def twitter_experiments():
 
     twitter = TwitterAPI(
         CONFIG["twitter"]["api_key"],
@@ -70,31 +129,6 @@ def tweet_picture(file_path):
     )
     print (response.status_code)
 
-    # Post a picture:
-
-    print('Tweeting picture ...')
-
-    # step 1: upload image:
-    file = open(file_path, 'rb')
-    picture = file.read()
-    response = twitter.request(
-        'media/upload',
-        None,
-        { 'media': picture }
-    )
-    print (response.status_code)
-
-    # step 2: tweet status with reference to uploaded picture:
-    if response.status_code == 200:
-        media_id = response.json()['media_id']
-        response = twitter.request(
-            'statuses/update',
-            {
-                'status': 'Hello World! This is a picture :-)',
-                 'media_ids': media_id
-            }
-        )
-        print (response.status_code)
 
 ### MAIN ###
 
@@ -104,4 +138,23 @@ file_path = take_picture()
 
 status = upload_picture(file_path)
 
-status = tweet_picture(file_path)
+status = tweet_picture(file_path, 'Original image tweet')
+print(status)
+if status["error"]:
+    print("Error: ", status["response"]["errors"])
+    exit()
+print(status["response"]["id"])
+
+status = tweet_picture(file_path, 'Reply image tweet 1', status["response"]["id"])
+if status["error"]:
+    print("Error: ", status["response"]["errors"])
+    exit()
+print(status["response"]["id"])
+
+status = tweet_picture(file_path, 'Reply image tweet 2', status["response"]["id"])
+if status["error"]:
+    print("Error: ", status["response"]["errors"])
+    exit()
+print(status["response"]["id"])
+
+#twitter_experiments()
