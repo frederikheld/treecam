3a604ab9aee4e757b0961698626e6ceb30581e2d
Frederik Held <git@frederikheld.de>
2023-04-21 02:33:51 +0200

    Fixes transfer script

--- tools/ftp-transfer/transfer_old.sh
+++ tools/ftp-transfer/transfer_old.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-# Downloads files from the server ftp server that was
-# specified with the env variables
-#       - FTP_SERVER
-#       - FTP_DIR
-#       - FTP_USER
-#       - FTP_SECRET
-# Moves the files to folders that are named by date within
-# the directory given with the env variable
-#       - ARCHIVE_DIR
-# Deletes the files from the ftp server (only those that
-# were successfully downloaded and moved)
+# Downloads files from the server to which the camera uploads the image to the NAS.
+# Moves the files to folders that are named by date.
+# Deletes the files from the ftp server.
+
+export TEMP_DIR=./temp
+export TEMP_FILELIST=filelist.temp
+export TEMP_FILELIST_SUCCESSFUL=filelist-successful.temp
 
 # load environment variables:
 if [ -f .env ]; then export $(cat .env | xargs); fi
@@ -20,25 +16,19 @@ echo "  FTP_DIR=${FTP_DIR}"
 echo "  FTP_USER=${FTP_USER}"
 if [ -z $FTP_SECRET ]; then echo "  FTP_SECRET="; else echo "  FTP_SECRET=*****"; fi
 
-# create list of all files that are currently on the FTP server:
+# create list of all files on the FTP server:
 curl --user ${FTP_USER}:${FTP_SECRET} --ftp-ssl --ssl-reqd --ftp-method multicwd -l "ftp://${FTP_SERVER}/${FTP_DIR}/" | grep "^[^\.]" > ${TEMP_FILELIST}
 
-# DEBUG: remove all but 3 lines:
-sed -i '4,$ d' ${TEMP_FILELIST}
-
-# download all files in the list:
-mkdir ${TEMP_DOWNLOAD_DIR}
+# downloads all files in the list
+mkdir -p ${TEMP_DIR}
 while read filename
 do
-    echo "Downloading ftp://${FTP_SERVER}/${FTP_DIR}/${filename} to ${TEMP_DOWNLOAD_DIR}/${filename}"
-    curl --user "${FTP_USER}:${FTP_SECRET}" --ftp-ssl --ssl-reqd --ftp-method multicwd --output "${TEMP_DOWNLOAD_DIR}/${filename}" "ftp://${FTP_SERVER}/${FTP_DIR}/${filename}"
+    echo "Downloading ftp://${FTP_SERVER}/${FTP_DIR}/${filename} to ${TEMP_DIR}/${filename}"
+    curl --user "${FTP_USER}:${FTP_SECRET}" --ftp-ssl --ssl-reqd --ftp-method multicwd --output "${TEMP_DIR}/${filename}" "ftp://${FTP_SERVER}/${FTP_DIR}/${filename}"
 done < ${TEMP_FILELIST}
 
 # extracts dates from filenames
 file_dates_unique=$(cat ${TEMP_FILELIST} | sed -E 's/^([0-9\-]*)_.*$/\1/' | sort -u)
-
-# DEBUG: print dates:
-echo "unique dates: ${file_dates_unique}"
 
 # creates folders for each date (if not exist)
 echo "${file_dates_unique}" | {
@@ -53,12 +43,12 @@ echo "${file_dates_unique}" | {
 while read filename
 do
     target_dir=$(echo "${filename}" | sed -E 's/^([0-9\-]*)_.*$/\1/')
-    mv "${TEMP_DOWNLOAD_DIR}/${filename}" "${ARCHIVE_DIR}/${target_dir}/${filename}"
+    mv "${TEMP_DIR}/${filename}" "${ARCHIVE_DIR}/${target_dir}/${filename}"
     if [ $? -eq 0 ]
     then
         echo ${filename} >> ${TEMP_FILELIST_SUCCESSFUL}
     else
-        echo "ERROR: moving ${TEMP_DOWNLOAD_DIR}/${filename} to ${ARCHIVE_DIR}/${target_dir}/${filename} failed! Please check manually!"
+        echo "ERROR: moving ${TEMP_DIR}/${filename} to ${ARCHIVE_DIR}/${target_dir}/${filename} failed! Please check manually!"
     fi
 done < ${TEMP_FILELIST}
 
@@ -66,7 +56,7 @@ done < ${TEMP_FILELIST}
 while read filename
 do
     echo "Deleting ${FTP_DIR}/${filename} from FTP server"
-    curl --user "${FTP_USER}:${FTP_SECRET}" --silent --show-error --ftp-ssl --ssl-reqd --ftp-method multicwd --quote "DELE ${FTP_DIR}/${filename}" ftp://${FTP_SERVER}/${FTP_DIR}/ > /dev/null
+    curl --user "${FTP_USER}:${FTP_SECRET}" --ftp-ssl --ssl-reqd --ftp-method multicwd "ftp://${FTP_SERVER}/${FTP_DIR}" --quote "DELE /${FTP_DIR}/${filename}" 
 done < ${TEMP_FILELIST_SUCCESSFUL}
 
 # delete temporary lists
